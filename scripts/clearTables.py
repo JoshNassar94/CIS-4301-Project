46ca745cee7b9454b0cf99c0191d505a3daab357
Joshua Nassar <jnassar@stormx.cise.ufl.edu>
2016-10-25 15:20:59 -0400

    Fixed clearTables

--- scripts/clearTables.py
+++ scripts/clearTables.py
@@ -1,43 +1,43 @@
 filename = "clearTables.sql"
 open(filename, 'w').close()
 SQL = open(filename, 'w+')
-query = "Delete from table ClassRoster where ClassID>-1;"
+query = "Delete from ClassRoster where ClassID>-1;"
 SQL.write(query + "\n")
 
-query = "Delete from table IntramuralTeamRoster where TeamID>-1;"
+query = "Delete from IntramuralTeamRoster where TeamID>-1;"
 SQL.write(query + "\n")
 
-query = "Delete from table IntramuralGame where GameID>-1;"
+query = "Delete from IntramuralGame where GameID>-1;"
 SQL.write(query + "\n")
 
-query = "Delete from table Class where ClassID>-1;"
+query = "Delete from Class where ClassID>-1;"
 SQL.write(query + "\n")
 
-query = "Delete from table EquipmentTransaction where TransactionID>-1;"
+query = "Delete from EquipmentTransaction where TransactionID>-1;"
 SQL.write(query + "\n")
 
-query = "Delete from table Equipment where EquipmentID>-1;"
+query = "Delete from Equipment where EquipmentID>-1;"
 SQL.write(query + "\n")
 
-query = "Delete from table CheckIn where CheckInID>-1;"
+query = "Delete from CheckIn where CheckInID>-1;"
 SQL.write(query + "\n")
 
-query = "Delete from table Guest where GuestID>-1;"
+query = "Delete from Guest where GuestID>-1;"
 SQL.write(query + "\n")
 
-query = "Delete from table UfAffiliate where UFID>-1;"
+query = "Delete from UfAffiliate where UFID>-1;"
 SQL.write(query + "\n")
 
-query = "Delete from table Acitvity where ActivityID>-1;"
+query = "Delete from Acitvity where ActivityID>-1;"
 SQL.write(query + "\n")
 
-query = "Delete from table IntramuralTeam where TeamID>-1;"
+query = "Delete from IntramuralTeam where TeamID>-1;"
 SQL.write(query + "\n")
 
-query = "Delete from table Facility where FacilityID>-1;"
+query = "Delete from Facility where FacilityID>-1;"
 SQL.write(query + "\n")
 
-query = "Delete from table Person where PersonID>-1;"
+query = "Delete from Person where PersonID>-1;"
 SQL.write(query + "\n")
 
 
