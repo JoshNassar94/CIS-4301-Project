94e6c6fe185d42d6f980abc4fe628d30cb897ad1
Joshua Nassar <jnassar@lin113-07.cise.ufl.edu>
2016-11-01 11:38:04 -0400

    Basic login stuff

---
+++ webpage/script.js
@@ -0,0 +1,15 @@
+function login(form) {
+	var ufid = $("#ufid").val();
+	var password1 = $("#password").val();
+	$.post("login.php", { ufid: ufid, password1: password1 },
+		function(data) {
+			if(data == 1){
+				window.location.href = "http://cise.ufl.edu/~jnassar/recsports+/account-info";
+			}
+			else{
+				alert("Incorrect UFID or password!");
+				return false;
+			}
+	});
+  return true;
+}
